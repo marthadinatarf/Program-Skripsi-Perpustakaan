408a4e943e42ae1877735a60461d0e329c821896
Rizki Firdaus <rizki.achanation@gmail.com>
2022-04-11 11:27:20 +0700

    Backup Program Skripsi

---
+++ src/koneksi.java
@@ -0,0 +1,35 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rizki_
+ */
+public class koneksi {
+    public static Connection getConnection() {
+        Connection connection = null;
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/perpustakaan"; //ganti dengan database mu
+        String user = "root";
+        String password = "";
+        if (connection == null) {
+            try {
+                Class.forName(driver);
+                connection = DriverManager.getConnection(url, user, password);
+            } catch (ClassNotFoundException | SQLException error) {
+               System.exit(0);
+            }
+
+        }
+        return connection;
+    }
+    
+}
